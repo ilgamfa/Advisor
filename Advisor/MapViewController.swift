3bd43f71a68c325adc4cb0af589ba160db937e50
ilgamfa <ilgamfa@yandex.ru>
2021-09-28 19:07:17 +0300

    add map

--- Advisor/MapViewController.swift
+++ Advisor/MapViewController.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    private let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Map"
-        view.backgroundColor = .blue
+        view.backgroundColor = .cyan
+
+        checkLocationServices()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkAuthorizationStatus()
+        }
+        else {
+            // Show alert letting the user know they have to turn this on.
+        }
     }
-    */
+    
+    private func checkAuthorizationStatus() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+        case .denied: // Show alert telling users how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
+        case .restricted: // Show an alert letting them know what’s up
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
 
 }
+
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion.init(center: center, span: span)
+    }
+}
+
