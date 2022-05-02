34e9150dba9960591349a2588dac465d8c45d257
ilgamfa <ilgamfa@yandex.ru>
2022-05-02 18:53:54 +0200

    added scroll view on detail view controller

---
+++ Advisor-V2/Advisor/Advisor/Screen/Detail/DetailView.swift
@@ -0,0 +1,29 @@
+//
+//  DetailView.swift
+//  Advisor_V2
+//
+//  Created by Ильгам Ахматдинов on 02.05.2022.
+//
+
+import UIKit
+
+class DetailView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
